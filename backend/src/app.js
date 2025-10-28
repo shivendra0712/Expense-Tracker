77d3b0d7c3b9b9dc54b3ebdadd0fedc292bc96ce
shivendra0712 <patelshivendra817@gmail.com>
2025-10-28 22:22:44 +0530

    added middleware , controller, model , route files

---
+++ backend/src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import expenseRoutes from './routes/expenses.js';
+import authRoutes from './routes/auth.js';
+
+const app = express();
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+// Routes
+app.use('/api/expenses', expenseRoutes);
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Expense Tracker API' });
+});
+
+export default app;
